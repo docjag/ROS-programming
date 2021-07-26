43d18bfaed449bd7a8f205bbf6d029a038c8d9a0
Sohel Mahmud <sohel.digitalhive@gmail.com>
2021-07-26 18:13:29 -0400

    Add test file

---
+++ scripts/test_freq_pub.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import sys
+import rospy
+import random
+import rostest
+import unittest
+from std_msgs.msg import Float32
+
+
+PKG = 'test_roslaunch'
+
+
+class TestFreqPub(unittest.TestCase):
+
+    publish_ok = False
+
+    def callback_func(self):
+        self.publish_ok = True
+
+    
+    def test_publish_frequency(self):
+        rospy.init_node('test_freq_pub_node')
+        rospy.Subscriber('frequency', Float32, self.callback_func)
+
+        counter = 0
+        while not rospy.is_shutdown() and count < 5 and (not self.publish_ok):
+            sleep(1)
+            counter += 1
+
+if __name__ == '__main__':
+    rostest.rosrun("bedrock_ocean_assign", 'test_freq_pub_node', TestFreqPub)
